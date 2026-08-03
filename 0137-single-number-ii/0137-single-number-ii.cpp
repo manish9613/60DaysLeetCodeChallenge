89524ba64916db7fb8db813613e3b200b6318b0c
Manish <mani.sh.965170@gmail.com>
2026-08-03 23:16:05 +0530

    Time: 3 ms (31.57%), Space: 13.4 MB (65.58%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
+    // int singleNumber(vector<int>& nums) {
+
+    // Brute force
+    // int n = nums.size();
+    // unordered_map<int, int>mp;
+    // for(auto it:nums){
+    //     mp[it]++;
+    // }
+    // int ans = 0;
+    // for(auto x : mp){
+    //     if(x.second == 1){
+    //         ans =  x.first;
+    //     }
+    // }
+    // return ans;
     int singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int, int>mp;
-        for(auto it:nums){
-            mp[it]++;
-        }
         int ans = 0;
-        for(auto x : mp){
-            if(x.second == 1){
-                ans =  x.first;
+
+        for (int i = 0; i < 32; i++) {
+
+            int cnt = 0;
+
+            for (int x : nums) {
+
+                if ((x >> i) & 1)
+                    cnt++;
             }
+
+            if (cnt % 3)
+                ans |= (1 << i);
         }
+
         return ans;
     }
 };
