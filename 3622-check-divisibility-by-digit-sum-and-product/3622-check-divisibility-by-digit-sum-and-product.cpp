2cfc745afb7cde291b0684f9710a8d804209d7a2
Manish <mani.sh.965170@gmail.com>
2026-08-22 12:55:25 +0530

    Time: 0 ms (100%), Space: 7.6 MB (97.17%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
@@ -4,17 +4,18 @@ public:
         int m = n;
         int sum = 0;
         int prd = 1;
+
         while (n != 0) {
             int digit = n % 10;
-            sum += digit;
-            prd *= digit;
+            sum = sum + digit;
+            prd = prd * digit;
             n = n / 10;
         }
-        int div = m % (sum + prd);
-        if (div == 0) {
+        int add = sum + prd;
+        if (m % add == 0) {
             return true;
         } else {
             return false;
-        }
+        } 
     }
 };
